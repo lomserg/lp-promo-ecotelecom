0115d62a27fa2ea10794cf732877aae2b9a35de1
lomserg <lomserg@gmail.com>
2024-04-01 14:29:53 +0300

    feat: add swiper to tarif class

--- src/assets/data/tarif-class.ts
+++ src/assets/data/tarif-class.ts
@@ -1,6 +1,7 @@
 import tarifData, { TarifType } from "./tarifs-data";
 import ElementCreator from "../utils/create-element";
 import { Params } from "../utils/create-element";
+import Swiper from "swiper";
 
 class Modal extends ElementCreator {
   parentElement: HTMLElement;
@@ -37,7 +38,10 @@ const CARD = {
   tag: "div",
   classNames: ["card", "swiper-slide"],
 };
-
+const CONTAINER = {
+  tag: "div",
+  classNames: "swiper",
+};
 // const OPTIONS = {
 //   tag: "p",
 //   classNames: "tarif-options",
@@ -55,25 +59,45 @@ export default class Tarifs {
   parentElement: HTMLElement;
   modal: Modal | null;
   tv: boolean;
+  swiperClassName: string;
   // Constructor
   constructor(
     data: TarifType[],
     classNames: string | string[],
     parentElement: HTMLElement,
-    tv: boolean
+    tv: boolean,
+    swiperClassName: string
   ) {
     this.data = data;
     this.classNames = classNames;
     this.parentElement = parentElement;
     this.modal = null;
     this.tv = tv;
+    this.swiperClassName = swiperClassName;
+    this.populate();
   }
 
   populate() {
+    // Create Swiper container
+    const swiperContainer = new ElementCreator(CONTAINER);
+    const swiperElem = swiperContainer.getElement() as HTMLDivElement;
+
+    // Create Swiper wrapper
+    const swiperWrapper = new ElementCreator({
+      tag: "div",
+      classNames: "swiper-wrapper",
+    });
+    const swiperWrapperElem = swiperWrapper.getElement() as HTMLDivElement;
+
+    // Append swiper wrapper to swiper container
+    swiperElem.appendChild(swiperWrapperElem);
+
+    // Populate cards within swiper wrapper
+    let hasCards = false; // Track if any cards are appended
     this.data.forEach((tarif) => {
       const card = new ElementCreator(CARD);
       if (card) {
-        const cardElem: HTMLDivElement = card.getElement() as HTMLDivElement; // Asserting type to HTMLDivElement
+        const cardElem: HTMLDivElement = card.getElement() as HTMLDivElement;
 
         const button = new ElementCreator({
           tag: "button",
@@ -81,12 +105,16 @@ export default class Tarifs {
           textContent: "Подключить",
         }).getElement() as HTMLButtonElement;
 
-        if (this.tv === tarif.tv && tarif.channels) {
+        if (
+          (this.tv === tarif.tv && tarif.channels) ||
+          (!this.tv && !tarif.tv)
+        ) {
           const tarifName = new ElementCreator({
             tag: "div",
             classNames: "tarif-name",
             textContent: tarif.name,
           }).getElement() as HTMLDivElement;
+
           const tarifSpeed = new ElementCreator({
             tag: "p",
             classNames: "tarif-speed",
@@ -105,11 +133,12 @@ export default class Tarifs {
             textContent: tarif.description,
           }).getElement() as HTMLDivElement;
 
-          // Check if this.tv and tarif.tv are both true
           const tvChannels = new ElementCreator({
             tag: "p",
             classNames: "tarif-channels",
-            textContent: tarif.channels.toString() + " ТВ-каналов",
+            textContent: tarif.channels
+              ? tarif.channels.toString() + " ТВ-каналов"
+              : "",
           }).getElement() as HTMLParagraphElement;
 
           cardElem.append(
@@ -120,65 +149,47 @@ export default class Tarifs {
             tarifDescription,
             button
           );
-        }
 
-        if (this.tv === false && tarif.tv === false) {
-          const tarifName = new ElementCreator({
-            tag: "div",
-            classNames: "tarif-name",
-            textContent: tarif.name,
-          }).getElement() as HTMLDivElement;
-          const tarifSpeed = new ElementCreator({
-            tag: "p",
-            classNames: "tarif-speed",
-            textContent: tarif.speed.toString() + " " + "Мбит/c",
-          }).getElement() as HTMLParagraphElement;
+          // Add event listener to the button
+          button.addEventListener("click", () => {
+            if (this.modal === null) {
+              this.modal = new Modal(
+                this.parentElement,
+                tarif.name,
+                {} as Params
+              );
+              this.modal.open();
+            } else {
+              this.modal.close();
+              this.modal = new Modal(
+                this.parentElement,
+                tarif.name,
+                {} as Params
+              );
+              this.modal.open();
+            }
+          });
 
-          const tarifPrice = new ElementCreator({
-            tag: "div",
-            classNames: "tarif-price",
-            textContent: tarif.price.toString() + " руб.",
-          }).getElement() as HTMLDivElement;
-
-          const tarifDescription = new ElementCreator({
-            tag: "div",
-            classNames: "tarif-description",
-            textContent: tarif.description,
-          }).getElement() as HTMLDivElement;
-
-          cardElem.append(
-            tarifName,
-            tarifPrice,
-            tarifSpeed,
-            tarifDescription,
-            button
-          );
-        }
-
-        // Add event listener to the button
-        button.addEventListener("click", () => {
-          // Create and open modal when button is clicked
-          if (this.modal === null) {
-            this.modal = new Modal(
-              this.parentElement,
-              tarif.name,
-              {} as Params
-            );
-            this.modal.open();
-          } else {
-            this.modal.close();
-            this.modal = new Modal(
-              this.parentElement,
-              tarif.name,
-              {} as Params
-            );
-            this.modal.open();
-          }
-        });
-        if (cardElem.children.length > 0) {
-          this.parentElement.append(cardElem);
+          swiperWrapperElem.appendChild(cardElem);
+          hasCards = true; // Set flag to true if a card is appended
         }
       }
     });
+
+    // Check if any cards were appended, if not, remove swiper container
+    if (!hasCards) {
+      swiperElem.remove();
+    } else {
+      // Append swiper container to the parent element
+      this.parentElement.appendChild(swiperElem);
+
+      // Initialize Swiper
+      const swiper = new Swiper(swiperElem, {
+        // Your Swiper options here
+        slidesPerView: 3,
+        spaceBetween: 5,
+        loop: false,
+      });
+    }
   }
 }
