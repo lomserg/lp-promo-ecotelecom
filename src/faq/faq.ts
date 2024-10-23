0b999f8a9dfd8049df176565acbf4fd55c4910c3
lomserg <lomserg@gmail.com>
2024-10-23 15:12:26 +0300

    feat: add faq

--- src/faq/faq.ts
+++ src/faq/faq.ts
@@ -1,14 +1,46 @@
+import "./faq.css";
+const appElement = document.querySelector<HTMLDivElement>("#app");
+
 function getFaqData() {
-  fetch("../../public/data/faq.json")
+  fetch("/data/faq.json")
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data);
+      if (appElement) makeFaq(appElement, data);
+    })
     .catch((err) => console.log(err));
 }
-
+getFaqData();
 const makeFaqContainerElement = () => {
   const faqContainer = document.createElement("div");
-  faqContainer.classList.add("faqContainer");
+  faqContainer.classList.add("faq-container");
   return faqContainer;
+};
+
+const makeFaqItemElement = (faqItem: { question: string; answer: string }) => {
+  const { question, answer } = faqItem;
+  console.log(`Question: ${question}, Answer: ${answer}`);
+  const faqItemElement = document.createElement("div");
+  const faqTitleElement = document.createElement("p");
+  const faqContentElement = document.createElement("p");
+  const faqTogleBtnElement = document.createElement("button");
+
+  faqTitleElement.textContent = question;
+  faqContentElement.innerHTML = answer;
+
+  faqItemElement.classList.add("faq-item");
+  faqTitleElement.classList.add("faq-item__title");
+  faqContentElement.classList.add("faq-item__content");
+  faqTogleBtnElement.classList.add("faq-item__togle");
+
+  faqTitleElement.appendChild(faqTogleBtnElement);
+  faqItemElement.appendChild(faqTitleElement);
+  faqItemElement.appendChild(faqContentElement);
+
+  faqTogleBtnElement.addEventListener("click", () => {
+    faqItemElement.classList.toggle("open");
+  });
+  return faqItemElement;
 };
 
 const makeFaq = (
@@ -17,6 +49,10 @@ const makeFaq = (
 ) => {
   const container = makeFaqContainerElement();
   console.log(faqItems);
+  faqItems.forEach((item) => {
+    const faqItemElement = makeFaqItemElement(item);
+    container.append(faqItemElement);
+  });
   parenElement.appendChild(container);
 };
 export default makeFaq;
