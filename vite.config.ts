677f4cb77c700dea79762cf3c13d3c8d63078032
lomserg <lomserg@gmail.com>
2024-11-25 21:08:33 +0300

    feat: add mail php file

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import { defineConfig } from "vite";
 // Vite configuration
 export default defineConfig({
   // Base public path when served in production (adjust if deploying to a subfolder)
-  base: "",
+  base: "/vite/",
   // base: "/vite/",
   // Build options
   build: {
     outDir: "dist", // Output directory for build files
     sourcemap: true, // Enable source maps for easier debugging in production
+    rollupOptions: {
+      input: {
+        main: "index.html", // Main entry point
+        detail: "detail.html", // Additional entry point
+      },
+    },
   },
 
   // Server configuration (for development mode)
