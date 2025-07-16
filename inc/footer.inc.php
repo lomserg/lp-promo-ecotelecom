d53da8abc7dc1f260f7da8fa2d25f877419965db
unknown <lomserg@gmail.com>
2025-07-16 09:06:28 +0300

    fix: andata

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -22,87 +22,13 @@
 </footer>
 <!-- Andata Tag Manager Code -->
 <script type="application/javascript"
-    src="//tagmanager.andata.ru/api/v1/container/9b91d137-d2ac-4f60-8303-e5be7e7bbb96/published/code.js"></script>
+    src="https://tagmanager.hrke.andata.ru/api/v1/container/9b91d137-d2ac-4f60-8303-e5be7e7bbb96/published/code.js">
+</script>
 <!-- End Andata Tag Manager Code -->
 
-<!-- BEGIN JIVOSITE CODE {literal} -->
-<script type="text/javascript">
-(function() {
-    document.jivositeloaded = 0;
-    var widget_id = "dIrCWbDFXD";
-    var d = document;
-    var w = window;
+<script src="//code.jivo.ru/widget/dIrCWbDFXD" async></script>
 
-    function l() {
-        var s = d.createElement("script");
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "//code.jivosite.com/script/widget/" + widget_id;
-        var ss = document.getElementsByTagName("script")[0];
-        ss.parentNode.insertBefore(s, ss);
-    } //эта строка обычная для кода JivoSite
-    function zy() {
-        //удаляем EventListeners
-        if (w.detachEvent) {
-            //поддержка IE8
-            w.detachEvent("onscroll", zy);
-            w.detachEvent("onmousemove", zy);
-            w.detachEvent("ontouchmove", zy);
-            w.detachEvent("onresize", zy);
-        } else {
-            w.removeEventListener("scroll", zy, false);
-            w.removeEventListener("mousemove", zy, false);
-            w.removeEventListener("touchmove", zy, false);
-            w.removeEventListener("resize", zy, false);
-        }
-        //запускаем функцию загрузки JivoSite
-        if (d.readyState == "complete") {
-            l();
-        } else {
-            if (w.attachEvent) {
-                w.attachEvent("onload", l);
-            } else {
-                w.addEventListener("load", l, false);
-            }
-        }
-        //Устанавливаем куку по которой отличаем первый и второй хит
-        var cookie_date = new Date();
-        cookie_date.setTime(cookie_date.getTime() + 60 * 60 * 28 * 1000); //24 часа для Москвы
-        d.cookie =
-            "JivoSiteLoaded=1;path=/;expires=" + cookie_date.toGMTString();
-    }
-    if (d.cookie.search("JivoSiteLoaded") < 0) {
-        //проверяем, первый ли это визит на наш сайт, если да, то назначаем EventListeners на события прокрутки, изменения размера окна браузера и скроллинга на ПК и мобильных устройствах, для отложенной загрузке JivoSite.
-        if (w.attachEvent) {
-            // поддержка IE8
-            w.attachEvent("onscroll", zy);
-            w.attachEvent("onmousemove", zy);
-            w.attachEvent("ontouchmove", zy);
-            w.attachEvent("onresize", zy);
-        } else {
-            w.addEventListener("scroll", zy, {
-                capture: false,
-                passive: true
-            });
-            w.addEventListener("mousemove", zy, {
-                capture: false,
-                passive: true,
-            });
-            w.addEventListener("touchmove", zy, {
-                capture: false,
-                passive: true,
-            });
-            w.addEventListener("resize", zy, {
-                capture: false,
-                passive: true
-            });
-        }
-    } else {
-        zy();
-    }
-})();
-</script>
-<!-- {/literal} END JIVOSITE CODE -->
+
 <script type="text/javascript">
 !(function() {
     var t = document.createElement("script");
@@ -154,18 +80,7 @@ _tmr.push({
 <!-- /Top.Mail.Ru counter -->
 <script src="https://unpkg.com/@popperjs/core@2"></script>
 <script src="https://unpkg.com/tippy.js@6"></script>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-J64FEQC199"></script>
-<script>
-window.dataLayer = window.dataLayer || [];
 
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag("js", new Date());
-
-gtag("config", "G-J64FEQC199");
-</script>
 </body>
 
 </html>
